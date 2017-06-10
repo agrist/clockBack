eb95ee73b3c789a9b2f4073157f9758823faaa62
agrist <agris.traskovs@venta.lv>
2017-06-10 14:47:56 +0300

    overall updates to server, post not yet completed, additional dependencies added

--- routes.js
+++ routes.js
@@ -1,5 +1,8 @@
 module.exports = function(app) {
-  const pug = require('pug');
+    const pug = require('pug');
+    var fs = require('fs');
+    var bodyParser = require('body-parser');
+    var youtubedl = require('youtube-dl');
     /*
         var song = require('./controllers/song');
         app.get('/song', song.findAll);
@@ -7,45 +10,85 @@ module.exports = function(app) {
         app.put('/song/:id', song.update);
         app.delete('/song/:id', song.delete);
 */
-  //      var device = require('./controllers/device');
-  //      app.get('/device', device.findAll);
-  //      app.post('/device', device.add);
-  //      app.put('/user/:id', user.update);
-  //      app.delete('/user/:id', user.delete);
+    //      var device = require('./controllers/device');
+    //      app.get('/device', device.findAll);
+    //      app.post('/device', device.add);
+    //      app.put('/user/:id', user.update);
+    //      app.delete('/user/:id', user.delete);
 
     app.get('/', function(req, res) {
-      res.sendFile(__dirname + "/simple.html");
+        res.sendFile(__dirname + "/simple.html");
     });
 
-    app.get('/hfile', function(req, res) {
+    app.get('/hfile', function(req, res) { // example for specific file filled template
         res.send(pug.renderFile('views/index.pug', {
-            title: 'Hey DOn',
+            title: 'Hey Don',
             message: ' It works key!',
-            item: {a:'abc', b:'before', c:'continue'},
+            item: {
+                a: 'abc',
+                b: 'before',
+                c: 'continue',
+                nevermind_me: ' but this works'
+            },
             im: "Cats-ear1-996x1024.jpg"
         }));
     });
 
-    app.get('/radio', function(req, res){
-      //test with single http://80.232.162.149:8000/plus96mp3.m3u -> radio swh
-      var radio =
-    { alarm_on: true,
-      alarm_at: "11:11",
-      tone: 'http://80.232.162.149:8000/plus96mp3.m3u'};
-      radio.last_modified = new Date().toJSON();//.slice(0,10).replace(/-/g,'/');
-  //    radio.update_song -?
+    app.get('/radio', function(req, res) {
+        //test with single http://80.232.162.149:8000/plus96mp3.m3u -> radio swh
+        var radio = {
+            alarm_on: true,
+            alarm_at: "11:11",
+            radio: false,
+            tone: 'http://80.232.162.149:8000/plus96mp3.m3u'
+        };
+        radio.last_modified = new Date().toJSON(); //.slice(0,10).replace(/-/g,'/');
+        //    radio.update_song -?
 
-      res.send(        radio      );
+        res.send(radio);
     });
-    app.get('/radio2', function(req, res){
-      //test with single http://80.232.162.149:8000/plus96mp3.m3u -> radio swh
-      var radio =
-    { alarm_on: true,
-      alarm_at: "11:11",
-      tone: 'https://www.youtube.com/watch?v=LkbJ90wwbO8'};
-      radio.last_modified = new Date().toJSON();//.slice(0,10).replace(/-/g,'/');
 
-      res.send(        radio      );
+    app.get('/radio2', function(req, res) {
+        var radio = {
+            alarm_on: true,
+            alarm_at: "11:11",
+            radio: false,
+            tone: 'https://www.youtube.com/watch?v=LkbJ90wwbO8'
+        };
+        radio.last_modified = new Date().toJSON();
+
+        res.send(radio);
     });
+/*
+  //  var jsonParser = bodyParser.json();
+//    var urlencodedParser = bodyParser.urlencoded({ extended: false });
+    app.post('/setyoutube', function(req, res) {
+    console.log(req.body);
+    //  if (!req.body) return res.sendStatus(400);
+
+
+    var device = req.body.device || 'def';
+    var youtube_link = req.body.link || 'https://www.youtube.com/watch?v=IH8RVvIHB9E';
+    /*    var video = youtubedl(youtube_link,
+            // Optional arguments passed to youtube-dl.
+            ['--audio-format m4a']
+            // Additional options can be given for calling `child_process.execFile()`.
+            );
+
+        // Will be called when the download starts.
+        console.log('Device: ' , device);
+        video.on('info', function(info) {
+            console.log('Download started');
+            console.log('filename: ' + info.filename);
+            console.log('size: ' + info.size);
+*/
+//                res.send('all ok');
+      //  });
+
+//        video.pipe(fs.createWriteStream('/staticFolder/alarmtested.m4a'));
+        //set info to return from some file - temp db replacement
+      //  res.send('all ok');
+
+//    });
 
 };
--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 var express = require("express");
+var bodyParser = require('body-parser');
 var app = express();
+var multer = require('multer'); 
+var upload = multer(); // for parsing multipart/form-data
 var hostname = '127.0.0.1',
-    port = 80;//remember to change to 80 for QA
+    port = 8090;//remember to change to 80 for QA
 require('./routes')(app);
 const pug = require('pug'); // database setup for mysq, need a setup
+
 
 app.engine('html', require('pug').renderFile);
 
 app.set('view engine', 'pug');
 
 app.use(express.static('staticFolder')); // static folder for music files
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 var server = app.listen(port, function() {
     console.log("Example app listening at http://%s:%s", hostname, port);
 });
