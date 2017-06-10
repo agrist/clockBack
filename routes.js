e6381383fdcc2bb59c37f2c0a353620022593ade
agrist <agris.traskovs@venta.lv>
2017-06-10 16:26:23 +0300

    links to radio

--- routes.js
+++ routes.js
@@ -3,21 +3,9 @@ module.exports = function(app) {
     var fs = require('fs');
     var bodyParser = require('body-parser');
     var youtubedl = require('youtube-dl');
-    /*
-        var song = require('./controllers/song');
-        app.get('/song', song.findAll);
-        app.post('/song', song.add);
-        app.put('/song/:id', song.update);
-        app.delete('/song/:id', song.delete);
-*/
-    //      var device = require('./controllers/device');
-    //      app.get('/device', device.findAll);
-    //      app.post('/device', device.add);
-    //      app.put('/user/:id', user.update);
-    //      app.delete('/user/:id', user.delete);
-
+    var jsonParser = bodyParser.json();
     app.get('/', function(req, res) {
-        res.sendFile(__dirname + "/simple.html");
+        res.render("index.html");
     });
 
     app.get('/hfile', function(req, res) { // example for specific file filled template
@@ -52,43 +40,44 @@ module.exports = function(app) {
         var radio = {
             alarm_on: true,
             alarm_at: "11:11",
-            radio: false,
+            play_radio: false,
+            radio: 'http://us3.internet-radio.com:8007/listen.pls&t=.pls',
             tone: 'https://www.youtube.com/watch?v=LkbJ90wwbO8'
         };
         radio.last_modified = new Date().toJSON();
 
         res.send(radio);
     });
-/*
-  //  var jsonParser = bodyParser.json();
-//    var urlencodedParser = bodyParser.urlencoded({ extended: false });
-    app.post('/setyoutube', function(req, res) {
-    console.log(req.body);
+
+
+
+
+    app.post('/setyoutube',jsonParser, function(req, res) {
+      console.log(req.form);
+      console.log("Link!!",req.body.link);
+//    console.log("could find body parsers", urlencodedParser);
     //  if (!req.body) return res.sendStatus(400);
-
-
     var device = req.body.device || 'def';
-    var youtube_link = req.body.link || 'https://www.youtube.com/watch?v=IH8RVvIHB9E';
-    /*    var video = youtubedl(youtube_link,
+    var youtube_link = req.body.link || 'def'; //'https://www.youtube.com/watch?v=IH8RVvIHB9E';
+    console.log(youtube_link);
+//        var video = youtubedl(youtube_link,
             // Optional arguments passed to youtube-dl.
-            ['--audio-format m4a']
+//            ['--audio-format m4a']
             // Additional options can be given for calling `child_process.execFile()`.
-            );
+//            );
 
         // Will be called when the download starts.
         console.log('Device: ' , device);
-        video.on('info', function(info) {
-            console.log('Download started');
-            console.log('filename: ' + info.filename);
-            console.log('size: ' + info.size);
-*/
-//                res.send('all ok');
-      //  });
+//        video.on('info', function(info) {
+//            console.log('Download started');
+//            console.log('filename: ' + info.filename);
+//            console.log('size: ' + info.size);
+//        });
 
-//        video.pipe(fs.createWriteStream('/staticFolder/alarmtested.m4a'));
+    //   video.pipe(fs.createWriteStream('staticFolder/alarmtested.m4a'));
         //set info to return from some file - temp db replacement
-      //  res.send('all ok');
+        res.send('all ok');
 
-//    });
+    });
 
 };
--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 var express = require("express");
 var bodyParser = require('body-parser');
-var app = express();
-var multer = require('multer');
-var upload = multer(); // for parsing multipart/form-data
+
 var hostname = '127.0.0.1',
-    port = 80;//remember to change to 80 for QA
-require('./routes')(app);
-const pug = require('pug'); // database setup for mysq, need a setup
+    port = 8090;//remember to change to 80 for QA
 
+const pug = require('pug');
+//var bodyParser = require('body-parser');
+//var multer = require('multer'); // v1.0.5
+//var upload = multer(); // for parsing multipart/form-data
 
+var app = express();
 app.engine('html', require('pug').renderFile);
-
 app.set('view engine', 'pug');
-
 app.use(express.static('staticFolder')); // static folder for music files
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(express.static('images')); // static folder for music files
+app.use(express.static('css')); // static folder for music files
 
+app.use(bodyParser.json()); // for parsing application/json
+//app.use(bodyParser.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
+
+
+require('./routes')(app);
 var server = app.listen(port, function() {
     console.log("Example app listening at http://%s:%s", hostname, port);
 });
