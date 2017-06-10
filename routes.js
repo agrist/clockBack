99fcae1d381c7efd2256c5f08403e9c87ed5fea7
agrist <agris.traskovs@venta.lv>
2017-06-10 09:32:51 +0300

    update with pug template set up via view folder

--- routes.js
+++ routes.js
@@ -1,23 +1,28 @@
 module.exports = function(app) {
-/*
-    var song = require('./controllers/song');
-    app.get('/song', song.findAll);
-    app.post('/song', song.add);
-    app.put('/song/:id', song.update);
-    app.delete('/song/:id', song.delete);
+  const pug = require('pug');
+    /*
+        var song = require('./controllers/song');
+        app.get('/song', song.findAll);
+        app.post('/song', song.add);
+        app.put('/song/:id', song.update);
+        app.delete('/song/:id', song.delete);
 
-    var user = require('./controllers/device');
-    app.get('/user', user.findAll);
-    app.post('/user', user.add);
-    app.put('/user/:id', user.update);
-    app.delete('/user/:id', user.delete);
-*/
+        var user = require('./controllers/device');
+        app.get('/user', user.findAll);
+        app.post('/user', user.add);
+        app.put('/user/:id', user.update);
+        app.delete('/user/:id', user.delete);
+    */
     app.get('/hello', function(req, res) {
-        res.send('Hello User, you are in the green! \n');
+        res.send(pug.renderFile('views/index.pug', {
+            title: 'Hey DOn',
+            message: ' It works key!',
+            im: "Cats-ear1-996x1024.jpg"
+        }));
     });
 
-    app.get('/hfile', function(req, res){
-      res.sendFile( __dirname + "/simple.html" );
+    app.get('/hfile', function(req, res) {
+        res.sendFile(__dirname + "/simple.html");
     });
 
 };
--- server.js
+++ server.js
@@ -2,19 +2,14 @@ var express = require("express");
 var app = express();
 var hostname = '127.0.0.1',
     port = 8090;
-require('./routes')(app), // database setup for mongo, need a setup script
-    mongoose = require('mongoose');
-//    mongoUrl = 'mongodb://localhost/clocks';
+require('./routes')(app);
+const pug = require('pug'); // database setup for mysq, need a setup
 
-/*
-mongoose.connect(mongoUrl);
-var db = mongoose.connection;
-db.on('error', function() {
-    throw new Error('unable to connect to database at ' + mongoUrl);
-});
-*/
+app.engine('html', require('pug').renderFile);
+
+app.set('view engine', 'pug');
+
 app.use(express.static('staticFolder')); // static folder for music files
-
 var server = app.listen(port, function() {
     console.log("Example app listening at http://%s:%s", hostname, port);
 });
